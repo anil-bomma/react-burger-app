be5743159506d3f01893da4a0f47ca23ed017d9c
anil-bomma <anil.bommaa@gmail.com>
2020-12-14 17:01:08 -0500

    added map object in react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import React, { Component } from "react";
+
+
 import Person from "./Person/Person";
 import Counter from "./Person/Counter";
 
-function App() {
-  return (
-    <div className="center">
-      <h1>Hello World!!</h1>
-      <p>This is working really</p>
-      <hr />
-      <Person name="Ab">Props Children</Person>
-      <Person name="Ac"/>
-      <Person name="Ad"/>
-      <hr />
-      <Counter name="Counter Stricker: "/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [{ name: "BB" }, { name: "Bc" }],
+  };
+
+  render() {
+    const personMap = this.state.persons.map((item) => (
+      <Person key={item.name} name={item.name}> map object</Person>
+    ));
+
+    return (
+      <div className="center">
+        <h1>Hello World!!</h1>
+        <p>This is working really</p>
+        <hr />
+        <Person name="Ab">Props Children</Person>
+        <Person name="Ac" />
+        <Person name="Ad" />
+        <hr />
+        <Counter name="Counter Stricker: " />
+        <hr />
+        {personMap}
+      </div>
+    );
+  }
 }
 
 export default App;
