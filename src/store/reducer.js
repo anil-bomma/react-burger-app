37563c631605c46b8516734090a365ef5612fde6
anil-bomma <anil.bommaa@gmail.com>
2020-12-20 18:35:16 -0500

    bug fix

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ const reducer = (state = { counter: 1 }, action) => {
 
     // must update state immutabily
     // similar to hook useState not like setState (not merge will create new state)
+    // 
     if (action.type == "INCEREMENT") {
         return {
             ...state,
